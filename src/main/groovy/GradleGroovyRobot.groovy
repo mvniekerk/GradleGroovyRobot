f40e4939904edfbdc253371b39b8db0b0d6c8a62
Sean Gilligan <sean@msgilligan.com>
2014-12-10 00:53:51 -0800

    Make it Groovy!
    * Remove some unnecessary type declarations, semicolons, use properties
    instead of getter/setters, etc.
    
    * Change button Title ;)

--- src/main/groovy/GradleGroovyRobot.groovy
+++ src/main/groovy/GradleGroovyRobot.groovy
@@ -1,20 +1,21 @@
-import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.foundation.NSDictionary;
-import org.robovm.apple.foundation.NSString;
-import org.robovm.apple.uikit.UIApplication;
-import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
-import org.robovm.apple.uikit.UIButton;
-import org.robovm.apple.uikit.UIButtonType;
-import org.robovm.apple.uikit.UIControlState;
-import org.robovm.apple.uikit.UIWindow;
-import org.robovm.objc.annotation.NotImplemented;
+import org.robovm.apple.foundation.NSAutoreleasePool
+import org.robovm.apple.foundation.NSDictionary
+import org.robovm.apple.foundation.NSString
+import org.robovm.apple.foundation.Foundation
+import org.robovm.apple.uikit.UIApplication
+import org.robovm.apple.uikit.UIApplicationDelegateAdapter
+import org.robovm.apple.uikit.UIButton
+import org.robovm.apple.uikit.UIButtonType
+import org.robovm.apple.uikit.UIControlState
+import org.robovm.apple.uikit.UIWindow
+import org.robovm.objc.annotation.NotImplemented
 import org.robovm.apple.uikit.UIControl
 import org.robovm.apple.uikit.UIEvent
 import org.robovm.apple.uikit.UIScreen
 import org.robovm.apple.uikit.UIColor
 import org.robovm.apple.uikit.UIApplicationLaunchOptions
 import org.robovm.apple.coregraphics.CGRect
-import groovy.transform.CompileStatic;
+import groovy.transform.CompileStatic
 
 @CompileStatic
 class GradleGroovyRobot extends UIApplicationDelegateAdapter {
@@ -25,24 +26,25 @@ class GradleGroovyRobot extends UIApplicationDelegateAdapter {
 	
 	
 	@Override
-	public boolean didFinishLaunching(UIApplication application,
+	boolean didFinishLaunching(UIApplication application,
 			UIApplicationLaunchOptions launchOptions) {
+        Foundation.log 'didFinishLaunching'
 		button = UIButton.create(UIButtonType.RoundedRect)
-		button.setFrame(new CGRect(115.0f, 121.0f, 91.0f, 37.0f))
-		button.setTitle("Click me!", UIControlState.Normal)
+		button.frame = new CGRect(80.0f, 121.0f, 180.0f, 37.0f)
+		button.setTitle('iOS, Groovy is in you!', UIControlState.Normal)
 		
-		window = new UIWindow(UIScreen.getMainScreen().getBounds());
-		window.setBackgroundColor(UIColor.lightGray());
-		window.addSubview(button);
-		window.makeKeyAndVisible();
+		window = new UIWindow(UIScreen.mainScreen.bounds)
+		window.backgroundColor = UIColor.lightGray()
+		window.addSubview(button)
+		window.makeKeyAndVisible()
 		return true
 	}
 			
 	@CompileStatic
 	static void main(String[] args) {
-		System.out.println "Squak"
-		NSAutoreleasePool pool = new NSAutoreleasePool();
-		UIApplication.main(args, GradleGroovyRobotApplication.class, GradleGroovyRobot.class);
-		pool.close();
+		System.out.println 'Squak'
+		def pool = new NSAutoreleasePool()
+		UIApplication.main(args, GradleGroovyRobotApplication.class, GradleGroovyRobot.class)
+		pool.close()
 	}
 }
